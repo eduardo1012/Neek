ace1639cd3a85dd42c6aeb4072e2a16198ae74a4
eduardo1012 <ediie1012@gmail.com>
2022-11-28 13:50:08 -0600

    Add files via upload

---
+++ cotiza001.js
@@ -0,0 +1,272 @@
+  
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  
+  <script>
+  
+  document.getElementById('ContinueButton').style.visibility='hidden';
+  
+      window.onload = function() 
+      {
+              var br1 = document.getElementById("BienesRaicesButton1");
+              br1.onclick = bienesraices1;
+  
+              var br2 = document.getElementById("BienesRaicesButton2");
+              br2.onclick = bienesraices2;
+  
+              var br3 = document.getElementById("BienesRaicesButton3");
+              br3.onclick = bienesraices3;
+  
+              var br4 = document.getElementById("BienesRaicesButton4");
+              br4.onclick = bienesraices4;
+  
+              var br5 = document.getElementById("BienesRaicesButton5");
+              br5.onclick = bienesraices5;
+  
+              var br6 = document.getElementById("BienesRaicesButton6");
+              br6.onclick = bienesraices6;
+  
+  
+  
+              var rt1 = document.getElementById("RetiroButton1");
+              rt1.onclick = retiro1;
+  
+              var rt2 = document.getElementById("RetiroButton2");
+              rt2.onclick = retiro2;
+  
+              var rt3 = document.getElementById("RetiroButton3");
+              rt3.onclick = retiro3;
+  
+              var rt4 = document.getElementById("RetiroButton4");
+              rt4.onclick = retiro4;
+  
+              var rt5 = document.getElementById("RetiroButton5");
+              rt5.onclick = retiro5;
+  
+              var rt6 = document.getElementById("RetiroButton6");
+              rt6.onclick = retiro6;            
+  
+  
+  
+  
+  
+              var ft1 = document.getElementById("FuturoButton1");
+              ft1.onclick = futuro1;
+  
+              var ft2 = document.getElementById("FuturoButton2");
+              ft2.onclick = futuro2;
+  
+              var ft3 = document.getElementById("FuturoButton3");
+              ft3.onclick = futuro3;
+  
+              var ft4 = document.getElementById("FuturoButton4");
+              ft4.onclick = futuro4;
+  
+              var ft5 = document.getElementById("FuturoButton5");
+              ft5.onclick = futuro5;
+  
+              var ft6 = document.getElementById("FuturoButton6");
+              ft6.onclick = futuro6;       
+  
+  
+  
+  
+              var ed1 = document.getElementById("EducacionButton1");
+              ed1.onclick = educacion1;
+  
+              var ed2 = document.getElementById("EducacionButton2");
+              ed2.onclick = educacion2;
+  
+              var ed3 = document.getElementById("EducacionButton3");
+              ed3.onclick = educacion3;
+  
+              var ed4 = document.getElementById("EducacionButton4");
+              ed4.onclick = educacion4;
+  
+              var ed5 = document.getElementById("EducacionButton5");
+              ed5.onclick = educacion5;
+  
+              var ed6 = document.getElementById("EducacionButton6");
+              ed6.onclick = educacion6;       
+  
+          }
+      function bienesraices1() {
+        radiobtn = document.getElementById("Bienes-Raices-1");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+        
+      }
+      function bienesraices2() {
+        radiobtn = document.getElementById("Bienes-Raices-2");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function bienesraices3() {
+        radiobtn = document.getElementById("Bienes-Raices-3");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function bienesraices4() {
+        radiobtn = document.getElementById("Bienes-Raices-4");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function bienesraices5() {
+        radiobtn = document.getElementById("Bienes-Raices-5");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function bienesraices6() {
+        radiobtn = document.getElementById("Bienes-Raices-6");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Bienes Raices 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+  
+  
+  
+  
+      function retiro1() {
+        radiobtn = document.getElementById("Retiro-1-2");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function retiro2() {
+        radiobtn = document.getElementById("Retiro-2");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function retiro3() {
+        radiobtn = document.getElementById("Retiro-3-4");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function retiro4() {
+        radiobtn = document.getElementById("Retiro-4-5");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function retiro5() {
+        radiobtn = document.getElementById("Retiro-5-6");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function retiro6() {
+        radiobtn = document.getElementById("Retiro-6-7");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Retiro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+  
+  
+      function futuro1() {
+        radiobtn = document.getElementById("Futuro-1");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function futuro2() {
+        radiobtn = document.getElementById("Futuro-2-3");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function futuro3() {
+        radiobtn = document.getElementById("Futuro-3-4");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function futuro4() {
+        radiobtn = document.getElementById("Futuro-4-5");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function futuro5() {
+        radiobtn = document.getElementById("Futuro-5-6");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function futuro6() {
+        radiobtn = document.getElementById("TipoDePlan1");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Futuro 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+  
+      
+  
+      function educacion1() {
+        radiobtn = document.getElementById("Educacion-1");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function educacion2() {
+        radiobtn = document.getElementById("Educacion-2");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function educacion3() {
+        radiobtn = document.getElementById("Educacion-3");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function educacion4() {
+        radiobtn = document.getElementById("Educacion-4");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function educacion5() {
+        radiobtn = document.getElementById("Educacion-5");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+      function educacion6() {
+        radiobtn = document.getElementById("Educacion-6");
+        radiobtn.checked = true;    
+        swal("Tu futuro esta a punto de cambiar", "Has seleccionado Educacion 1,500 por mes!", "success");
+        $('.w-round div:nth-child(2)').trigger('tap');
+        document.getElementById('ContinueButton').style.visibility='visible';
+      }
+  
+  
+  </script>

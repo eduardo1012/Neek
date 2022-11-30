e09901eb4eb943489cbb2be360e9256fedc33658
eduardo1012 <ediie1012@gmail.com>
2022-11-29 20:25:49 -0600

    Add files via upload

---
+++ cotiza001.js
@@ -0,0 +1,10 @@
+window.onload = function() 
+  {
+    var br1 = document.getElementById("Confirmar-Enviar");
+    br1.onclick = bienesraices1;
+  }
+
+  
+  function retiro1() {
+    alert ("This is an alert dialog box");  
+  }

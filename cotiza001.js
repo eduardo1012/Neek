19bb14166829efd5ddab79a907a94ba55c34105f
eduardo1012 <ediie1012@gmail.com>
2022-11-29 20:24:59 -0600

    q
    a

--- cotiza001.js
+++ cotiza001.js
@@ -1,1 +1,10 @@
-alert ("This is an alert dialog box");  
+window.onload = function() 
+  {
+    var br1 = document.getElementById("Confirmar-Enviar");
+    br1.onclick = bienesraices1;
+  }
+
+  
+  function retiro1() {
+    alert ("This is an alert dialog box");  
+  }
